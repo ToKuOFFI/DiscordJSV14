e8a37bf7eb9b14fa9868e4376158edff3f0a4402
ToKuOFFI <110974588+ToKuOFFI@users.noreply.github.com>
2022-08-27 23:18:32 +0200

    Update DataBase.js

--- DataBase.js
+++ DataBase.js
@@ -1,22 +1,15 @@
 const mysql = require("mysql")
 
-module.exports = async toku => {
+const db = mysql.createConnection({
+   host: "NOM DE VOTRE DATABASE",
+   user: ""NOM DE L'UTILSATEUR ( "root" si local host )",
+   password: "MDP DE VOTRE DATABASE ( rien mettre si local host )",
+   database: 'NOM DE LA DATABASE',
+ });
 
-    const Database = new mysql.createConnection({
-        host: "NOM DE VOTRE DATABASE",
-        user: "NOM DE L'UTILSATEUR ( "root" si local host )",
-        password: "MDP DE VOTRE DATABASE ( rien mettre si local host )",
-        database: "NOM DE LA DATABASE"
-    })
-    
-    Database.connect(function(err) {
-        if (err) {
-            console.error('error connecting: ' + err.stack);
-            return;
-        }
-    
-        console.log("📋 Base de données bien relié.")
-    })
-    
-    module.exports = Database;
-}
+ db.connect(function(err) {
+   if (err) throw err;
+   console.log("📋 Base de données bien relié.");
+ });
+
+module.exports = db
