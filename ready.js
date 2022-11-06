cd004c0db7b5c8598da40453da4b3b7906b5b547
ToKuOFFI <110974588+ToKuOFFI@users.noreply.github.com>
2022-11-06 16:00:24 +0100

    Create ready.js

---
+++ ready.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js")
+const loadSlashCommands = require("../Structures/loadSlashCommands")
+const loadDataBase = require('../Structures/loadDataBase');
+
+module.exports = async toku => {
+  /* Permet de savoir sur quelle serveur est le bot ! */
+  toku.guilds.fetch().then(
+        (_guilds) => {
+          let owners = new Map();
+          toku.guilds.cache.forEach(({ ownerId, id, name, memberCount }) => {
+            if (!owners.get(ownerId)) owners.set(ownerId, { count: 1, guilds: [{ id, name, memberCount: memberCount || 0  }]})
+            else {
+              const data = owners.get(ownerId);
+              owners.set(ownerId, ({ count: data.count + 1, guilds: [...data.guilds, ({ id, name, memberCount: memberCount || 0 }) ] }))
+            }
+          });
+      
+          [...owners]
+            .filter(([_, { count }]) => count > 1)
+            .forEach(([id, { count, guilds }]) => console.log(`L utilisateur ${id} a le bot sur ${count} serveurs.\nServeurs:\n  -${guilds.map((g) => g.id + " -- " + g.memberCount + " -- " + g.name).join("\n       -")}`));
+        },
+        () => console.log("Cannot fetch guilds")
+      );
+}
